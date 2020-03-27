2f8936cc81dca97770b0304d4dc862f17d3e4e82
xiote <xiote@nate.com>
2020-03-28 05:48:13 +0900

    PadLeft, PadRight 추가

--- stringutil/main.go
+++ stringutil/main.go
@@ -1,39 +1,23 @@
-package strutil
+package stringutil
 
-import (
-	"unicode"
-)
+import ()
 
-func IsEmpty(s string) bool {
-	if s == "" {
-		return true
-	} else {
-		return false
-	}
-}
-
-func IsSpaceOrEmpty(s string) bool {
-	return IsEmptyS(s)
-}
-
-// check that string is empty or space.
-func IsEmptyS(s string) bool {
-	return IsEmpty(s) || IsSpace(s)
-}
-
-func IsNotEmpty(s string) bool {
-	return !IsEmpty(s)
-}
-
-func IsSpace(s string) bool {
-	for _, char := range s {
-		if !unicode.IsSpace(char) {
-			return false
+func PadRight(str, pad string, length int) string {
+	length--
+	for {
+		if len(str) > length {
+			return str
 		}
+		str += pad
 	}
-	return true
 }
 
-func IsNotSpace(s string) bool {
-	return !IsSpace(s)
+func PadLeft(str, pad string, length int) string {
+	length--
+	for {
+		if len(str) > length {
+			return str
+		}
+		str = pad + str
+	}
 }
--- stringutil/main_test.go
+++ stringutil/main_test.go
@@ -1,105 +1,39 @@
-package strutil
+package stringutil
 
 import (
 	"testing"
 )
 
-func TestIsEmpty(t *testing.T) {
+func TestPadLeft(t *testing.T) {
 	cases := []struct {
-		inStr       string
-		wantIsEmpty bool
+		in1  string
+		in2  string
+		in3  int
+		want string
 	}{
-		{"NotEmptyString", false},
-		{"", true},
+		{"12", "a", 4, "aa12"},
 	}
 	for _, c := range cases {
-		gotIsEmpty := IsEmpty(c.inStr)
-		if gotIsEmpty != c.wantIsEmpty {
-			t.Errorf("IsEmpty(%q) == %v, want %v", c.inStr, gotIsEmpty, c.wantIsEmpty)
-		}
-	}
-}
-
-func TestIsSpaceOrEmpty(t *testing.T) {
-	cases := []struct {
-		inStr              string
-		wantIsSpaceOrEmpty bool
-	}{
-		{"NotSpaceOrEmptyString", false},
-		{" NotSpaceOrEmptyString ", false},
-		{"", true},
-		{"  ", true},
-	}
-	for _, c := range cases {
-		gotIsSpaceOrEmpty := IsSpaceOrEmpty(c.inStr)
-		if gotIsSpaceOrEmpty != c.wantIsSpaceOrEmpty {
-			t.Errorf("IsSpaceOrEmpty(%q) == %v, want %v", c.inStr, gotIsSpaceOrEmpty, c.wantIsSpaceOrEmpty)
-		}
-	}
-}
-
-func TestIsEmptyS(t *testing.T) {
-	cases := []struct {
-		in   string
-		want bool
-	}{
-		{"NotSpaceOrEmptyString", false},
-		{" NotSpaceOrEmptyString ", false},
-		{"", true},
-		{"  ", true},
-	}
-	for _, c := range cases {
-		got := IsEmptyS(c.in)
+		got := PadLeft(c.in1, c.in2, c.in3)
 		if got != c.want {
-			t.Errorf("IsEmptyS(%q) == %v, want %v", c.in, got, c.want)
+			t.Errorf("PadLeft(%q, %q, %d) == %q, want %q", c.in1, c.in2, c.in3, got, c.want)
 		}
 	}
 }
 
-func TestIsNotEmpty(t *testing.T) {
+func TestPadRight(t *testing.T) {
 	cases := []struct {
-		in   string
-		want bool
+		in1  string
+		in2  string
+		in3  int
+		want string
 	}{
-		{"", false},
-		{"  ", true},
+		{"12", "k", 4, "12kk"},
 	}
 	for _, c := range cases {
-		got := IsNotEmpty(c.in)
+		got := PadRight(c.in1, c.in2, c.in3)
 		if got != c.want {
-			t.Errorf("IsNotEmpty(%q) == %v, want %v", c.in, got, c.want)
-		}
-	}
-}
-
-func TestIsSpace(t *testing.T) {
-	cases := []struct {
-		in   string
-		want bool
-	}{
-		{"  ", true},
-		{" abc  ", false},
-	}
-	for _, c := range cases {
-		got := IsSpace(c.in)
-		if got != c.want {
-			t.Errorf("IsSpace(%q) == %v, want %v", c.in, got, c.want)
-		}
-	}
-}
-
-func TestIsNotSpace(t *testing.T) {
-	cases := []struct {
-		in   string
-		want bool
-	}{
-		{"  ", false},
-		{" abc  ", true},
-	}
-	for _, c := range cases {
-		got := IsNotSpace(c.in)
-		if got != c.want {
-			t.Errorf("IsNotSpace(%q) == %v, want %v", c.in, got, c.want)
+			t.Errorf("PadRight(%q, %q, %d) == %q, want %q", c.in1, c.in2, c.in3, got, c.want)
 		}
 	}
 }
