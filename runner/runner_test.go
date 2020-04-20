17b5c32baf80b645c00c39500985f9125001d7a5
xiote <xiote@nate.com>
2020-04-21 04:50:38 +0900

    update

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -10,6 +10,10 @@ func (y YourT1) Concat(input1 string, input2 string) (string, error) {
 	return input1 + input2, nil
 }
 
+func (y YourT1) Get(input1 string) error {
+	return nil
+}
+
 func TestCall(t *testing.T) {
 	inMethodName := "Concat"
 	inString1 := "A"
@@ -21,5 +25,16 @@ func TestCall(t *testing.T) {
 
 	if result != want {
 		t.Errorf("Call(%q) == %v, want %v", inMethodName, result, want)
+	}
+}
+
+func TestCall2(t *testing.T) {
+	inMethodName := "Get"
+
+	r := NewRunner(YourT1{})
+	result, _ := r.Call("Get", []string{"http://play.golang.org/?simple=1"})
+
+	if result != "" {
+		t.Errorf("Call(%q) == %v, want %v", inMethodName, result, "")
 	}
 }
